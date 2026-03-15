c5af77234ba713cff69d3eeaedab3f52b27c18ce
SantyMG03 <santymorenog2003@gmail.com>
2026-03-15 11:42:59 +0100

    Updated Task to allow field validation

--- src/main/java/com/santy/taskorg/models/Task.java
+++ src/main/java/com/santy/taskorg/models/Task.java
@@ -1,7 +1,17 @@
 package com.santy.taskorg.models;
 
+import jakarta.validation.constraints.FutureOrPresent;
+import jakarta.validation.constraints.NotNull;
+import org.springframework.format.annotation.DateTimeFormat;
+
 public class Task {
+
+    @NotNull(message = "Title cannot be empty")
     private String title;
+
+    @NotNull(message = "Please select a date")
+    @FutureOrPresent(message = "Date must be today or in the future")
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
     private String dueDate;
 
     public Task() {}
