2f6f93b949bdf73f0c75ab228388c6d87a6e3fb9
SantyMG03 <santymorenog2003@gmail.com>
2026-03-15 12:17:48 +0100

    Solved bug where you can create an event without title

--- src/main/java/com/santy/taskorg/models/Task.java
+++ src/main/java/com/santy/taskorg/models/Task.java
@@ -1,6 +1,7 @@
 package com.santy.taskorg.models;
 
 import jakarta.validation.constraints.FutureOrPresent;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import org.springframework.format.annotation.DateTimeFormat;
 
@@ -8,7 +9,7 @@ import java.time.LocalDate;
 
 public class Task {
 
-    @NotNull(message = "Title cannot be empty")
+    @NotBlank(message = "Title cannot be empty")
     private String title;
 
     @NotNull(message = "Please select a date")
